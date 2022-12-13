4434f9ec333ee0be8fdbf4980a86fa2f06227720
Dinh Bang <ndinhbang111@gmail.com>
2022-12-13 23:23:05 +0700

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import dns from 'dns'
 
-// https://vitejs.dev/config/
+// @see: https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim')
+
+// @see: https://vitejs.dev/config/
 export default defineConfig(
   ({command, mode, ssrBuild}) => {
     // load .env
@@ -96,6 +100,7 @@ export default defineConfig(
         }
       },
       server: {
+        strictPort: true,
         https: {
           key: process.env.VITE_APP_SERVER_KEY,
           cert: process.env.VITE_APP_SERVER_CERT,
